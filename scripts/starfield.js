3e3b809b9b0759bbb8b66d52f84f85c3591a6ec9
Jeremy Larter <jeremylarter@live.com.au>
2016-09-11 22:44:37 +1000

    Fix todos. Minor refactoring.

--- scripts/starfield.js
+++ scripts/starfield.js
@@ -12,9 +12,9 @@
         starStream = Rx.Observable.range(1, numberOfStars)
             .map(function () {
                 return {
-                    x: parseInt(Math.random() * canvasWidth),//todo: what is better parseInt or Math.floor?
-                    y: parseInt(Math.random() * canvasHeight),
-                    size: Math.random() * 3 + 1//todo: check the effect if size is not an integer
+                    x: Math.floor(Math.random() * canvasWidth),
+                    y: Math.floor(Math.random() * canvasHeight),
+                    size: Math.random() * 3 + 1
                 };
             })
             .toArray()
@@ -29,21 +29,21 @@
                         });
                         return starArray;
                     });
-            })
-            .subscribe(function (starArray) {
-                function paintStars(starArray, context) {
-                    context.fillStyle = "#000000";
-                    context.fillRect(0, 0, canvasWidth, canvasHeight);
-                    context.fillStyle = "#ffffff";
-                    starArray.forEach(function (star) {
-                        context.fillRect(star.x, star.y, star.size, star.size);
-                    });
-                }
-                paintStars(starArray, context);
             });
 
+    starStream.subscribe(function (starArray) {
+        function paintStars(starArray, context) {
+            context.fillStyle = "#000000";
+            context.fillRect(0, 0, canvasWidth, canvasHeight);
+            context.fillStyle = "#ffffff";
+            starArray.forEach(function (star) {
+                context.fillRect(star.x, star.y, star.size, star.size);
+            });
+        }
+        paintStars(starArray, context);
+    });
 
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
     document.body.appendChild(canvas);
-} (window, document, Rx));
+}(window, document, Rx));
