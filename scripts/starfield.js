da97488648482826f4c2a0e360449e1ebac8338f
Jeremy Larter <jeremylarter@live.com.au>
2016-09-17 14:36:48 +1000

    inject random numbers

--- scripts/starfield.js
+++ scripts/starfield.js
@@ -1,15 +1,22 @@
 /*jslint browser:true */
-/*global window, Rx*/
-(function (window, document, Rx) {
+/*global window, Rx, Random*/
+
+//injected library functions and objects that cause side effects in order to create good test seams
+var StarField = {};
+
+//StarField.getRandom = Random.getRandom;
+StarField.getRandom = Math.random;
+
+(function (window, document, Rx, getRandom) {
     "use strict";
 
     var canvasWidth = window.innerWidth,
         canvasHeight = window.innerHeight,
         canvas = document.createElement("canvas"),
-        context = canvas.getContext("2d"),
 
-        //todo: make options a behavior subject so they can change with the game state over time
         options = {
+            scoreSubject: new Rx.BehaviorSubject(0),
+            context: canvas.getContext("2d"),
             starArray: {
                 speed: 40,
                 number: 250
@@ -19,8 +26,8 @@
                 boundingBoxSize: 25
             },
             enemyBulletArray: {
-                rate: 1500,
-                number: 100,
+                rate: 200,
+                number: 1000,
                 firing: {
                     speed: 12
                 }
@@ -42,6 +49,7 @@
                 boundingBoxSize: 20
             }
         },
+        optionsSubject = new Rx.BehaviorSubject(options),
 
         starBehavior = function (star) {
             if (star.y >= canvasHeight) {
@@ -61,12 +69,15 @@
             }
         },
         //todo: wrap and inject all instances of functions with side effects such as Math.random, keyboard and mouse input
+        //maybe we can zip to streams that use random().
+        //randomStream = Rx.Observable.from([getRandom(), getRandom(), getRandom(), getRandom(), getRandom(), getRandom(), getRandom(), getRandom()]),
+
         starStream = Rx.Observable.range(1, options.starArray.number)
             .map(function () {
                 return {
-                    x: Math.floor(Math.random() * canvasWidth),
-                    y: Math.floor(Math.random() * canvasHeight),
-                    size: Math.random() * 3 + 1,
+                    x: Math.floor(getRandom() * canvasWidth),
+                    y: Math.floor(getRandom() * canvasHeight),
+                    size: getRandom() * 3 + 1,
                     grow: true
                 };
             })
@@ -82,7 +93,7 @@
         enemyStream = Rx.Observable.interval(options.enemyArray.rate)
             .scan(function (enemyArray) {
                 enemyArray.push({
-                    x: Math.random() * canvasWidth,
+                    x: getRandom() * canvasWidth,
                     y: canvasHeight / 2,
                     alive: true,
                     value: 5
@@ -140,22 +151,21 @@
                 return bulletArray;
             }, []),
 
-        scoreSubject = new Rx.BehaviorSubject(0),
-        scoreStream = scoreSubject
+        scoreStream = options.scoreSubject
             .scan(function (previous, current) {
                 return previous + current;
             }, 0),
 
-        gameStreamArray = [starStream, enemyStream, enemyBulletStream, bulletStream, spaceShipPositionStream, scoreStream],
-        gameCombineLatest = function (starArray, enemyArray, enemyBulletArray, bulletArray, spaceShipPosition, score) {
+        gameStreamArray = [optionsSubject, starStream, enemyStream, enemyBulletStream, bulletStream, spaceShipPositionStream, scoreStream],
+        gameCombineLatest = function (options, starArray, enemyArray, enemyBulletArray, bulletArray, spaceShipPosition, score) {
             return {
+                options: options,
                 starArray: starArray,
                 enemyArray: enemyArray,
                 enemyBulletArray: enemyBulletArray,
                 bulletArray: bulletArray,
                 spaceShipPosition: spaceShipPosition,
-                score: score,
-                context: context
+                score: score
             };
         },
         game = Rx.Observable.combineLatest(gameStreamArray, gameCombineLatest);
@@ -194,7 +204,7 @@
     }
 
     function decideToFire(chance) {
-        return Math.random() < chance;
+        return getRandom() < chance;
     }
 
     function paintEnemies(enemyArray, enemyBulletArray, context) {
@@ -272,13 +282,16 @@
     }
 
     function renderScene(actors) {
+        var scoreSubject = actors.options.scoreSubject,
+            context = actors.options.context;
+
         paintBackground(context);
-        paintStars(actors.starArray, actors.context);
-        paintEnemies(actors.enemyArray, actors.enemyBulletArray, actors.context);
-        paintEnemyBullets(actors.enemyBulletArray, actors.spaceShipPosition, scoreSubject, actors.context);
-        paintBullets(actors.bulletArray, actors.enemyArray, scoreSubject, actors.context);
-        paintSpaceShip(actors.spaceShipPosition, actors.context);
-        paintScore(actors.score, actors.context);
+        paintStars(actors.starArray, context);
+        paintEnemies(actors.enemyArray, actors.enemyBulletArray, context);
+        paintEnemyBullets(actors.enemyBulletArray, actors.spaceShipPosition, scoreSubject, context);
+        paintBullets(actors.bulletArray, actors.enemyArray, scoreSubject, context);
+        paintSpaceShip(actors.spaceShipPosition, context);
+        paintScore(actors.score, context);
     }
 
     game
@@ -288,4 +301,5 @@
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
     document.body.appendChild(canvas);
-}(window, document, Rx));
+}(window, document, Rx, StarField.getRandom));
+
